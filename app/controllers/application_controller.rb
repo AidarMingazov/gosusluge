10ccab39c9b67ba69effb5f406fa8feb4206fe9b
Aidar Mingazov <am@21-school.ru>
2021-09-10 22:32:02 +0400

    cetrs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
 
   def certone
   end
+
+  def certtwo
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/vaccine/cert/verify/7a5ea870-deea-4a4d-b942-65e9bca400e4", to: 'application#certone'
+  get "/vaccine/cert/verify/7a5ea870-deea-4a4d-b942-65e9bca400e3", to: 'application#certtwo'
 end
